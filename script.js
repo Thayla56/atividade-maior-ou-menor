7fedb4fc1b3fa5120df9629bce111e17f21d92a3
Thayla56 <138219986+Thayla56@users.noreply.github.com>
2024-11-04 12:08:39 -0300

    f

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarNumero() {
     const numero =
     document.getElementById('numero').valueAsN
     umber;
-
+}
 if (numero > 60){
     document.getElementById('resultado').innerText = ${numero} é maior que 60;} else if (numero < 60){
 
