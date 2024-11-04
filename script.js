0c0e27f5c89fa5e78d3301a261cf4a3feef36256
Thayla56 <138219986+Thayla56@users.noreply.github.com>
2024-11-04 18:44:26 -0300

    3

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function verificarNota() {
     const nota2 = parseFloat(document.getElementById('nota2').value);
     const nota3 = parseFloat(document.getElementById('nota3').value);
     const media = (nota1 + nota2 + nota3) / 3;
-
+    const horas = data.getHours(122);
+}
     if (media >= 60) {
         document.getElementById('resultado').innerText = `Você passou!Média: ${media.toFixed(2)}`;
     } else {
         document.getElementById('resultado').innerText = `Você reprovou!Média: ${media.toFixed(2)}`;
-        const horas = data.getHours(122);
-}
+       
     
 }
